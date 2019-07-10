41bc1d4f730967e23785e432ae1e4969f4bb613c
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-10 10:42:41 +0800

    optimize on main.js about using new grammar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-function isBarcodesValid (barcodes,database) {
-    var flag = false;
+const isBarcodesValid = (barcodes,database) => {
+    let flag = false;
     for(let i = 0,len = barcodes.length; i < len;i++){
         for(let j in database){
             if(database[j].id.match(barcodes[i])){
@@ -15,8 +15,8 @@ function isBarcodesValid (barcodes,database) {
     return flag;
 }
 
-function countPriceByBarcodes (barcodes,database){
-    var printInfo = new Object();
+const countPriceByBarcodes = (barcodes,database) => {
+    let printInfo = new Object();
     for(let i = 0,len = barcodes.length; i < len;i++){
         for(let j in database){
             if(database[j].id.match(barcodes[i])){
@@ -25,14 +25,14 @@ function countPriceByBarcodes (barcodes,database){
                 let temp = printInfo[name]+"";
                 if(temp.indexOf('-') == -1){
                     //第一次添加物品
-                    printInfo[name] = price+"-1";
+                    printInfo[name] = '${price}-1';
                 }
                 else{
                     //已经有这个物品
                     let count = parseInt(temp.split('-')[1]);
                     let sum = parseInt(temp.split('-')[0]);
-                    count = count + 1;
-                    printInfo[name] = price + "-" + count;
+                    count += 1;
+                    printInfo[name] = '${price}-${count}';
                 }
             }
         }
@@ -41,20 +41,20 @@ function countPriceByBarcodes (barcodes,database){
     return printInfo;
 }
 
-function printReceiptsByBarcodes(printInfo,isBarcodesValid){
+const printReceiptsByBarcodes = (printInfo,isBarcodesValid) => {
     if(!isBarcodesValid){
         return "[ERROR]:";
     }
-    var sum = 0;
-    var msg = "Receipts\n------------------------------------------------------------\n";
+    let sum = 0;
+    let msg = "Receipts\n------------------------------------------------------------\n";
     for(let i in printInfo){
         let temp = printInfo[i];
         let price = parseInt(temp.split('-')[0]);
         let count = parseInt(temp.split('-')[1]);
         sum += price*count;
-        msg += i + " "+price + " "+count+"\n";
+        msg += '${i} ${price} ${count}\n';
     }
-    msg += "------------------------------------------------------------\nPrice: "+sum;
+    msg += '------------------------------------------------------------\nPrice: ${sum}';
     console.log(msg);
     return msg;
 }
